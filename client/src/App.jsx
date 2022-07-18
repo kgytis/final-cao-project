e371081cb8078b2a0584f4235a8a82ba010227a7
kgytis <kaziukon.gytis@gmail.com>
2022-07-18 14:40:24 +0300

    NavBar reallocation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { Box, createTheme, ThemeProvider } from "@mui/material";
 import FrontPage from "./pages/FrontPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import NavBar from "./components/NavBar";
 import Question from "./pages/Question";
 
 function App() {
@@ -18,15 +17,23 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Box bgcolor={"background.default"} color={"text.primary"}>
-        <NavBar setMode={setMode} mode={mode} />
         <Routes>
           <Route
             path="/"
             element={<FrontPage mode={mode} setMode={setMode} />}
           />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/question/:id" element={<Question />} />
+          <Route
+            path="/login"
+            element={<Login mode={mode} setMode={setMode} />}
+          />
+          <Route
+            path="/register"
+            element={<Register mode={mode} setMode={setMode} />}
+          />
+          <Route
+            path="/question/:id"
+            element={<Question mode={mode} setMode={setMode} />}
+          />
         </Routes>
       </Box>
     </ThemeProvider>
